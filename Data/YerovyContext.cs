6efef9f6552d57747fcc9e0e77e73977537f4e90
Craxker <Craxker@DESKTOP-JKIJLSQ>
2026-04-02 17:47:35 -0500

    Agregar archivos de proyecto.

---
+++ Data/YerovyContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using YerovyInventoryService.Models;
+
+namespace YerovyInventoryService.Data
+{
+    public class YerovyContext : DbContext
+    {
+        public YerovyContext(DbContextOptions<YerovyContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Producto> Productos { get; set; }
+        public DbSet<Categoria> Categorias { get; set; }
+        public DbSet<Usuario> Usuarios { get; set; }
+    }
+}
